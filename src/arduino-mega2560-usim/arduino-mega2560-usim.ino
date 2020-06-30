29cd6df75e0b9d092a9eab8c66500343e15bbec1
dilshan <jayakody2000lk@gmail.com>
2020-06-30 21:39:18 +0530

    Add license notice and port mapping details.

--- src/arduino-mega2560-usim/arduino-mega2560-usim.ino
+++ src/arduino-mega2560-usim/arduino-mega2560-usim.ino
@@ -1,3 +1,22 @@
+//------------------------------------------------------------------------------
+// USim - Lightweight Microcontroller Simulator.
+// Emulator for Arduino MEGA 2560.
+// 
+// Copyright 2020 Dilshan R Jayakody.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//------------------------------------------------------------------------------
+
 #include "buildconfig.h"
 
 #include <SD.h>
--- src/arduino-mega2560-usim/arduinoMega2560.c
+++ src/arduino-mega2560-usim/arduinoMega2560.c
@@ -1,6 +1,36 @@
+//------------------------------------------------------------------------------
+// USim - Lightweight Microcontroller Simulator.
+// Emulator for Arduino MEGA 2560.
+// 
+// Copyright 2020 Dilshan R Jayakody.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//------------------------------------------------------------------------------
+
 #include <Arduino.h>
 
 #include "vmmem84.h"
+
+// PIC and Arduino / ATMEGA 2560 Port Mapping:
+// 
+// ----------|-------------------------------
+// PIC Port  |   Arduino / ATMEGA 2560 Port
+// ----------|-------------------------------
+// PORT A    |   PORT A
+// TRIS A    |   DDR A
+// PORT B    |   PORT C
+// TRIS B    |   DDR C
+// ----------|-------------------------------
 
 void setHardwareRegValue(unsigned char addr, unsigned char val)
 {
